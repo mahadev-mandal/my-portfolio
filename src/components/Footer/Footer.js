e07f4fb18ed7e028ee31bb53e41579f15ae79c8f
mahadev mandal <mandalmahadev5@gmail.comm>
2022-06-24 04:49:39 +0545

    card section added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import waveImg from '../../assets/images/wave.png'
-import Social from '../Social/Social';
+import React from "react";
+import { Box } from "@mui/material";
+import waveImg from "../../assets/images/wave.png";
+import Social from "../Social/Social";
 
 function Footer() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
-        position: 'relative',
+        display: "flex",
+        alignItems: "center",
+        position: "relative",
       }}
     >
       <img src={waveImg} alt="" width="100%" />
       <Box
         sx={{
-          position:'absolute',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
         }}
       >
         <Social />
       </Box>
-    </Box >
-  )
+    </Box>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/components/ServicesCard/ServicesCard.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+
+export default function ServicesCard({ imgPath, heading, content }) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 300,
+        border: "8px solid #fed79a",
+        borderRadius: 5,
+        textAlign: "center",
+        p: 5,
+      }}
+    >
+      <Box>
+        <img src={imgPath} alt="" style={{ width: "30%" }} />
+        <Typography gutterBottom variant="h5" component="div">
+          {heading}
+        </Typography>
+      </Box>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+        <Button size="small" sx={{ mt: 2 }}>
+          Learn More
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
